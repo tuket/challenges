c4ef3e507575e935a845e61ef2ce52ec665824fb
tuket <tuketet@gmail.com>
2018-12-01 00:24:53 +0100

    08 3 D

--- codejam/08/3/d.cpp
+++ codejam/08/3/d.cpp
@@ -5,13 +5,13 @@
 #include <algorithm>
 #include <cassert>
 
-static const int M = 10007;
+static const int64_t M = 10007;
 
 using namespace std;
 
 struct P
 {
-    int x, y;
+    int64_t x, y;
     bool operator==(const P& p)const
         { return x == p.x && y == p.y; }
     bool operator!=(const P& p)const
@@ -39,35 +39,35 @@ namespace std
     };
 }
 
-int powMod(unsigned x, unsigned n)
+int64_t powMod(int64_t x, int64_t n)
 {
-    unsigned res = 1;
-    unsigned b = 1 << (sizeof(unsigned)*8-2);
+    int64_t res = 1;
+    int64_t b = 1LL << (sizeof(int64_t)*8LL-2LL);
     while(b != 0)
     {
         res = (res * res) % M;
         if(b & n)
             res = (res * x) % M;
-        b >>= 1;
+        b >>= 1LL;
     }
     return res;
 }
 
-int binCoef(int k, int n)
+int64_t binCoef(int64_t k, int64_t n)
 {
-    int me = 0; // number of times you can divide k! by M
-    int pm = M;
+    int64_t me = 0; // number of times you can divide k! by M
+    int64_t pm = M;
     while(pm <= k)
     {
         me += k / pm;
         pm *= M;
     }
 
-    int used = 0;
-    int a = 1;  // numerator
-    for(int i=n-k+1; i<=n; i++)
+    int64_t used = 0;
+    int64_t a = 1;  // numerator
+    for(int64_t i=n-k+1; i<=n; i++)
     {
-        int ii=i;
+        int64_t ii=i;
         while(used < me && ii % M == 0) {
             ii /= M;
             used++;
@@ -75,14 +75,14 @@ int binCoef(int k, int n)
         a = (a*ii) % M;
     }
 
-    int b = 1;  // denominator
-    for(int i=1; i<=k; i++)
+    int64_t b = 1;  // denominator
+    for(int64_t i=1; i<=k; i++)
     {
-        int ii=i;
+        int64_t ii=i;
         while(ii % M == 0) {
             ii /= M;
         }
-        b = (b*ii) % M;;
+        b = (b*ii) % M;
     }
 
     // invert b
@@ -91,7 +91,7 @@ int binCoef(int k, int n)
     return (a*b) % M;
 }
 
-int esol(P p)
+int64_t esol(P p)
 {
     return binCoef(p.x, p.x + p.y);
 }
@@ -99,32 +99,32 @@ int esol(P p)
 class Solver
 {
 public:
-    Solver(int W, int H, vector<P>&& rocks)
+    Solver(int64_t W, int64_t H, vector<P>&& rocks)
         : W(W), H(H), rocks(std::move(rocks))
     {
         for(const P& r : this->rocks)
             br.insert(r);
     }
 
-    uint64_t solve() { return solve(W, H); }
+    int64_t solve() { return solve(W, H); }
 
-    uint64_t solve(int X, int Y)
+    int64_t solve(int64_t X, int64_t Y)
     {
         if(X < 0 || Y < 0)
             return 0;
         if(X == 0 && Y == 0)
             return 1;
         
-        int res =  esol({X, Y});
-        struct SW { P r; int val; };
+        int64_t res =  esol({X, Y});
+        struct SW { P r; int64_t val; };
         vector<SW> layer;
         for(const P& r : rocks)
         {
-            int val = esol({r.x, r.y}) * esol({X-r.x, Y-r.y}) % M;
+            int64_t val = esol({r.x, r.y}) * esol({X-r.x, Y-r.y}) % M;
             res = (M + res - val) % M;
             layer.push_back({r, esol({r.x, r.y})});
         }
-        int sign = +1;
+        int64_t sign = +1;
         while(layer.size())
         {
             vector<SW> newLayer;
@@ -135,7 +135,7 @@ public:
                 {
                     if(sw.r.isParent(r))
                     {
-                        int val = (sw.val * esol({r.x - sw.r.x, r.y - sw.r.y})) % M;
+                        int64_t val = (sw.val * esol({r.x - sw.r.x, r.y - sw.r.y})) % M;
                         res = (M + res + sign * (val * esol({X-r.x, Y-r.y}))%M) % M;
                         newLayer.push_back({r, val});
                     }
@@ -148,33 +148,33 @@ public:
     }
 
 private:
-    int W, H;
+    int64_t W, H;
     vector<P> rocks;
     unordered_set<P> br;
 };
 
 int main()
 {
-    int nn;
+    int64_t nn;
     cin >> nn;
-    for(int kk=1; kk<=nn; kk++)
+    for(int64_t kk=1; kk<=nn; kk++)
     {
-        int W, H, R;
+        int64_t W, H, R;
         cin >> H >> W >> R;
 
         W--; H--;
-        int RW = (2*W - H) / 3;
-        int RH = (2*H - W) / 3;
+        int64_t RW = (2*W - H) / 3;
+        int64_t RH = (2*H - W) / 3;
         bool impossible = RW < 0 || RH < 0 || (2*W-H) % 3 != 0;
         
         vector<P> rocks;
-        for(int i=0; i<R; i++)
+        for(int64_t i=0; i<R; i++)
         {
-            int y, x;
+            int64_t y, x;
             cin >> y >> x;
             x--; y--;
-            int rx = (2*x - y) / 3;
-            int ry = (2*y - x) / 3;
+            int64_t rx = (2*x - y) / 3;
+            int64_t ry = (2*y - x) / 3;
             if (rx >= 0 && ry >= 0 && rx <= RW && ry <= RH && (2*x-y) % 3 == 0)
             {
                 rocks.push_back({rx, ry});
